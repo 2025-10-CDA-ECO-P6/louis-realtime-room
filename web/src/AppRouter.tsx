ad33a30c5a51b030786175c9c87e6bc8ee6a3c67
coljurten <louis.dalonis@gmail.com>
2026-02-19 16:15:12 +0100

    feat: :sparkles: Adding Chat page (MobileVersion)

--- web/src/AppRouter.tsx
+++ web/src/AppRouter.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './routes/Landing';
-// import Chat from './routes/Chat';
+import Chat from './routes/Chat';
 import './styles/index.css';
 
 const App: React.FC = () => {
@@ -9,7 +9,7 @@ const App: React.FC = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        {/* <Route path="/chat" element={<Chat />} /> */}
+        <Route path="/chat" element={<Chat />} />
       </Routes>
     </BrowserRouter>
   );
